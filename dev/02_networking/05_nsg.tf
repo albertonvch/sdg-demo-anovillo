63bf04feabc2775fe7b9b5a0a3d6990ba69f36dc
albertonvch <albertonvch@gmail.com>
2026-09-10 23:24:19 +0200

    Add outbound security rules for ACA to various Azure services in NSG

--- dev/02_networking/05_nsg.tf
+++ dev/02_networking/05_nsg.tf
@@ -101,5 +101,96 @@ module "dev_nsg" {
       source_address_prefix      = "*"
       destination_address_prefix = "*"
     }
+
+
+    AllowAcaToAcrOutbound = {
+      name                       = "AllowAcaToAcrOutbound"
+      access                     = "Allow"
+      destination_address_prefix = "MicrosoftContainerRegistry"
+      destination_port_range     = "443"
+      direction                  = "Outbound"
+      priority                   = 302
+      protocol                   = "Tcp"
+      source_address_prefixes    = local.subnets["aca"].address_prefixes
+      source_port_range          = "*"
+    }
+    AllowAcaToFrontDoorOutbound = {
+      name                       = "AllowAcaToFrontDoorOutbound"
+      access                     = "Allow"
+      destination_address_prefix = "AzureFrontDoor.FirstParty"
+      destination_port_range     = "443"
+      direction                  = "Outbound"
+      priority                   = 303
+      protocol                   = "Tcp"
+      source_address_prefixes    = local.subnets["aca"].address_prefixes
+      source_port_range          = "*"
+    }
+    AllowAcaToAcaOutbound = {
+      name                         = "AllowAcaToAcaOutbound"
+      access                       = "Allow"
+      destination_address_prefixes = local.subnets["aca"].address_prefixes
+      destination_port_range       = "*"
+      direction                    = "Outbound"
+      priority                     = 304
+      protocol                     = "Tcp"
+      source_address_prefixes      = local.subnets["aca"].address_prefixes
+      source_port_range            = "*"
+    }
+    AllowAcaToAadOutbound = {
+      name                       = "AllowAcaToAadOutbound"
+      access                     = "Allow"
+      destination_address_prefix = "AzureActiveDirectory"
+      destination_port_range     = "443"
+      direction                  = "Outbound"
+      priority                   = 305
+      protocol                   = "Tcp"
+      source_address_prefixes    = local.subnets["aca"].address_prefixes
+      source_port_range          = "*"
+    }
+    AllowAcaToAzureDnsUdpOutbound = {
+      name                       = "AllowAcaToAzureDnsUdpOutbound"
+      access                     = "Allow"
+      destination_address_prefix = "AzureActiveDirectory"
+      destination_port_range     = "53"
+      direction                  = "Outbound"
+      priority                   = 306
+      protocol                   = "Udp"
+      source_address_prefixes    = local.subnets["aca"].address_prefixes
+      source_port_range          = "*"
+    }
+    AllowAcaToAzureDnsTcpOutbound = {
+      name                       = "AllowAcaToAzureDnsTcpOutbound"
+      access                     = "Allow"
+      destination_address_prefix = "AzureActiveDirectory"
+      destination_port_range     = "53"
+      direction                  = "Outbound"
+      priority                   = 307
+      protocol                   = "Tcp"
+      source_address_prefixes    = local.subnets["aca"].address_prefixes
+      source_port_range          = "*"
+    }
+    AllowAcaToStorageOutbound = {
+      name                       = "AllowAcaToStorageOutbound"
+      access                     = "Allow"
+      destination_address_prefix = "Storage.WestEurope"
+      destination_port_range     = "53"
+      direction                  = "Outbound"
+      priority                   = 308
+      protocol                   = "Tcp"
+      source_address_prefixes    = local.subnets["aca"].address_prefixes
+      source_port_range          = "*"
+    }
+    AllowVnetAcaOutbound = {
+      name                       = "AllowVnetAcaOutbound"
+      access                     = "Allow"
+      destination_address_prefix = "Internet"
+      destination_port_ranges    = ["443", "587", "465"]
+      direction                  = "Outbound"
+      priority                   = 500
+      protocol                   = "Tcp"
+      source_address_prefix      = "10.7.0.0/23"
+      source_port_range          = "*"
+    }
+
   }
 }
