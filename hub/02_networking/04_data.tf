de689fda245a846832c2e87a0c0c65ec9b9f3c0b
albertonvch <albertonvch@gmail.com>
2026-09-10 19:58:21 +0200

    Fix resource group reference in log analytics workspace data source

--- hub/02_networking/04_data.tf
+++ hub/02_networking/04_data.tf
@@ -1,6 +1,6 @@
 data "azurerm_log_analytics_workspace" "law" {
   name                = "${var.prefix}-${var.project}-azsc1-law-${var.environment}-01"
-  resource_group_name = data.azurerm_resource_group.this.name
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 data "azurerm_resource_group" "rg" {
