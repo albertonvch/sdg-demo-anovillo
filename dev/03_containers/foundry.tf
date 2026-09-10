402d8487ac276a7449022ce8439353cc78b94250
albertonvch <albertonvch@gmail.com>
2026-09-10 02:18:50 +0200

    Refactor cognitive deployments section in foundry module; comment out gpt-4.1-mini configuration

--- dev/03_containers/foundry.tf
+++ dev/03_containers/foundry.tf
@@ -12,17 +12,17 @@ module "foundry" {
 
   sku_name = "S0"
   cognitive_deployments = {
-    "gpt-4.1-mini" = {
-      name = "gpt-4.1-mini"
-      model = {
-        format  = "OpenAI"
-        name    = "gpt-4.1-mini"
-        version = "2025-04-14"
-      }
-      scale = {
-        type = "Standard"
-      }
-    }
+    # "gpt-4.1-mini" = {
+    #   name = "gpt-4.1-mini"
+    #   model = {
+    #     format  = "OpenAI"
+    #     name    = "gpt-4.1-mini"
+    #     version = "2025-04-14"
+    #   }
+    #   scale = {
+    #     type = "Standard"
+    #   }
+    # }
   }
   enable_telemetry = false
   network_acls = {
@@ -36,6 +36,20 @@ module "foundry" {
     primary = {
       private_dns_zone_resource_ids = [data.azurerm_private_dns_zone.ai_openai.id]
       subnet_resource_id            = data.azurerm_subnet.integration.id
+    }
+  }
+  customer_managed_key = {
+    key_vault_resource_id = module.key_vault.resource_id
+    key_name              = local.cmk_dev_name
+    user_assigned_identity = {
+      resource_id = azurerm_user_assigned_identity.umi_dev.id
+    }
+  }
+
+  diagnostic_settings = {
+    to_law = {
+      name                  = "to-law"
+      workspace_resource_id = data.azurerm_log_analytics_workspace.hub_law.id
     }
   }
 }
